250b731b7fd12d9ade590e102918af675542eb29
idzenkov <id.zenkov@gmail.com>
2026-06-06 19:18:21 +0300

    conf: добавить конфигурацию OpenAPI для Swagger UI

---
+++ user-service/src/main/java/org/example/userservice/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package org.example.userservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .version("1.0")
+                        .description("CRUD operations for User management with Kafka events")
+                        .contact(new Contact()
+                                .name("Support")
+                                .email("support@example.com")));
+    }
+}
